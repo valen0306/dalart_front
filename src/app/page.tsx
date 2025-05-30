9ccd79d495223ad290e5931b40819f61380ec2fb
yui-119 <poppop0206@icloud.com>
2025-05-31 01:46:30 +0900

    headerのcomponents完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Footer from "./components/BottomNavigationBar"
+import Header from "./components/Header";
+import styles from "./route.module.css"
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.pageContainer}>
+      <Header />
       <div>hello</div>
       <Footer />
     </div>
