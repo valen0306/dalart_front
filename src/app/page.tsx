f8814cf17db41814f93dec36deb11c4189e9e8a6
yui-119 <poppop0206@icloud.com>
2025-06-22 17:31:40 +0900

    服のメイン画像表示コンポーネントの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Footer from "./components/BottomNavigationBar"
 import Header from "./components/Header"
 import styles from "./route.module.css"
-import UserIcon from './components/user-aikon'
+import UserIcon from './components/user-icon'
+import OutfitImage from "./components/FashionDisplay"
 
 export default function Home() {
   return (
     <div className={styles.pageContainer}>
       <Header />
       <UserIcon size={80} />
+      <OutfitImage ID="投稿画像" />
+      {/* <OutfitImage ID="投稿画像" BackscreenMode={true} /> */}
       <Footer />
     </div>
   );
