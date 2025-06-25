8a0003c646a51e2fe980ff0d23db8809941e5e8b
yui-119 <poppop0206@icloud.com>
2025-06-25 18:18:31 +0900

    navigationbarのアイコン色変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import Footer from "./components/BottomNavigationBar"
 import Header from "./components/Header"
 import styles from "./route.module.css"
 import UserIcon from './components/user-icon'
+import OutfitImage from './components/FashionDisplay'; 
+
 
 export default function Home() {
   return (
     <div className={styles.pageContainer}>
       <Header />
       <UserIcon size={80} />
+      <OutfitImage ID="投稿画像" />
       <Footer />
     </div>
   )
