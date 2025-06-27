d88b9d90a7eb8211e12aff13d4210902c4f2ce0a
yui-119 <poppop0206@icloud.com>
2025-06-27 18:29:07 +0900

    loading画面

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,41 @@
 'use client';
 
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Footer from './components/BottomNavigationBar';
 import Header from './components/Header';
 import styles from './route.module.css';
 import UserIcon from './components/user-icon';
 import OutfitImage from './components/FashionDisplay';
 import SplashScreen from './components/Splash';
+import Loading from './components/Loading';
 
 export default function Home() {
   const [showSplash, setShowSplash] = useState(true);
+  const [loading, setLoading] = useState(true);
+
+  // スプラッシュが終わったら次にLoadingへ
+  useEffect(() => {
+    if (!showSplash) {
+      const timer = setTimeout(() => setLoading(false), 2000); // API読み込みのふり
+      return () => clearTimeout(timer);
+    }
+  }, [showSplash]);
 
   if (showSplash) {
     return <SplashScreen onFinish={() => setShowSplash(false)} />;
   }
 
-  return (
-    <div className={styles.pageContainer}>
-      <Header />
-      <UserIcon size={80} />
-      <OutfitImage ID="投稿画像" />
-      <Footer />
-    </div>
-  );
-}
+  if (loading) {
+    return <Loading />;
+  }
+
+    return (
+      <div className={styles.pageContainer}>
+        <Header />
+        <UserIcon size={80} />
+        <OutfitImage ID="投稿画像" />
+        <Footer />
+      </div>
+    );
+  }
 
